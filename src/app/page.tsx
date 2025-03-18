948c3b67f712aec47c1f6f466c2849058e0275ab
Chingun <quartx@Chinguns-MacBook-Air.local>
2025-03-18 09:05:37 +0800

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,115 @@
-import Image from "next/image";
+'use client'
+
+
+import { useEffect, useState } from "react";
+import { ACCESS_TOKEN } from "./constants";
+import axios from "axios";
+import { Button } from "@/components/ui/button";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import { Film, Moon, Search } from "lucide-react";
+
+export type MovieType = {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string;
+  vote_average: number;
+  genre_ids: number[];
+  detail: {
+    desciption: string;
+
+  }
+  genre_ids_gen: Array<number>,
+
+
+
+}
+export type Actor = {
+  givenName: string,
+  movieName: string,
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [movieList, setMovieList] = useState<MovieType[]>([]);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const getMovies = async () => {
+    const movies = await axios.get(
+      "https://api.themoviedb.org/3/discover/movie",
+      {
+        headers: {
+          Authorization: `Bearer ${ACCESS_TOKEN}`,
+        },
+      }
+    );
+    setMovieList(movies.data.results)
+    // console.log(movies.data.results, 'here')
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return (
+    <div className="w-full">
+      <div className="flex justify-between">
+        <div className="flex">
+          <Film color="#4338CA" />
+          <h1 className="text-indigo-700"> Movie Z</h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="flex">
+          <div className="border w-[36px] h-[36px] flex justify-center items-center rounded-sm p-2">
+            <Search />
+          </div>
+          <div className="border w-[36px] h-[36px] flex justify-center items-center rounded-sm p-2">
+            <Moon />
+          </div>
+        </div>
+      </div>
+      <Carousel className="">
+        <CarouselContent>
+          {movieList.slice(0, 3).map((movie) => {
+            return (
+              <CarouselItem key={movie.id} className="">
+
+                
+                <div className="">
+                  <img className="" src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} />
+                </div>
+                <div className=" ">
+                <h1>{movie.title}</h1>
+                  <p>{movie.vote_average.toFixed(1)}/10</p>
+                  
+
+                  <p>{movie.overview}</p>
+
+                </div>
+              </CarouselItem>)
+
+
+          })}
+        </CarouselContent>
+        <CarouselPrevious className="absolute top-1/2 left-10"/>
+        <CarouselNext className="absolute top-1/2 right-10" />
+      </Carousel>
+      <Button>Watch Trailer</Button>
+
+      <div>
+        <h1>Upcoming</h1>
+
+      </div>
+
+
+
+
+
+
+
+
     </div>
   );
 }
